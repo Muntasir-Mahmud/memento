fb1b09bc2117a565e493a21c4350170362ec3008
Muntasir-Mahmud <m.siyam529@gmail.com>
2024-03-09 00:02:44 +0600

    Bug-fix: changed some linting

--- src/topics/models.py
+++ src/topics/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -10,10 +10,12 @@ from src.db import Base
 class Topic(Base):
     __tablename__ = "topics"
 
-    topic_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    topic_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,
+                      unique=True, nullable=False)
     name = Column(String(255), nullable=False)
     description = Column(String(255))
     parent_topic_id = Column(UUID(as_uuid=True), ForeignKey("topics.topic_id"))
 
-    children = relationship("Topic", cascade="all, delete-orphan", backref="parent")
+    children = relationship("Topic", cascade="all, delete-orphan",
+                            backref="parent")
     cards = relationship("Card", backref="topic")
